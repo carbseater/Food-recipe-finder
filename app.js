7b4a59b680c289a1820cfb37a04b818ab989c4f8
Madhav Mishra <madhavmishra28@gmail.com>
2020-09-01 17:05:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    request    = require('request')
+    app        = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//Routing 
+
+app.get('/', function (req, res) {
+    res.render('landing');
+});
+
+app.get('/home', function (req, res) {
+    res.render('home');
+});
+
+
+app.get("/result", function (req, res) {
+    var query = req.query.search;
+    var url = "https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=785125ae78a34bd78bbccf5f135f6da9";
+    request(url, function (error, response, body) {
+        var data = JSON.parse(body);
+        res.render('result', { data: data });
+    })
+});
+
+app.get('/recipe/:id', function (req, res) {
+    var query = req.params.id;
+    var url1 = "https://api.spoonacular.com/recipes/" + query + "/information?includeNutrition=false&apiKey=785125ae78a34bd78bbccf5f135f6da9";
+    // var url2 = "https://api.spoonacular.com/recipes/" + query + "/nutritionWidget?defaultCss=true";
+    request(url1, function (error, response, body1) {
+        var data = JSON.parse(body1);
+        res.render('recipe',{data:data});
+    })
+    
+});
+
+app.get('/nutrition/:id', function (req,res) {
+    var query = req.params.id;
+    var url = "https://api.spoonacular.com/recipes/" + query + "/nutritionWidget?defaultCss=true";
+    request(url, function (error, response, body1) {
+        res.render('nutrition',{body1:body1});
+    })
+})
+
+// app.get('/result', function (req, res) {
+//     res.render('result');
+// })
+
+app.listen(process.env.PORT, function () {
+    console.log("Server Running");
+});
